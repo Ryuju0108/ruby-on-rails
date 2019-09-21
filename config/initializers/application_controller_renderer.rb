59304882ba40ce2db737f0949c389440c39efb18
Ryuju <itou.ryuju.infratop@gmail.com>
2019-09-21 16:20:47 +0900

    finished chapter9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a31ee5c2002c1ae72d82e756d1353895a5a4e06a1cf9dc21c47c8ea0e24760c253d3e507e09e273e3ea24cf4b12212b842dd2c01e0bc97f66ede5cce0bb05718'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,16 @@ Rails.application.routes.draw do
 	get 'top' => 'root#top'
 	post 'todolists/todolists' => 'todolists#create'
 	# resources :todolists
+
+	get 'todolists' => 'todolists#index'
+
+	get 'todolists/:id' => 'todolists#show', as: 'todolist'
+	 # .../todolists/1 や .../todolists/3 に該当する
+
+	 get 'todolists/:id/edit' => 'todolists#edit', as:'edit/todolist'
+	 patch 'todolists/:id' => 'todolists#update', as:'update_todolist'
+
+	 delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
